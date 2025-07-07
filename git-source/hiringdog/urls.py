9b4d0252c99be90781a2b30aef7a5b13e036a21a
madhuri-9182 <madhurisomannagari@gmail.com>
2025-07-07 13:39:16 +0000

    mm

--- git-source/hiringdog/urls.py
+++ git-source/hiringdog/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
 from django.urls import path
+from .views import hello_world
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', hello_world),  # root URL
 ]
